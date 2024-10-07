c8f3cf71b8efa3bf5d79cd67f5209fb2bcc4550a
bgmer7 <caijinyang98@gmail.com>
2024-10-07 09:52:00 +0800

    calculate fund income

---
+++ quantfin/calculate_fund_income.py
@@ -0,0 +1,518 @@
+# 计算两支公募基金的收益
+
+# 005051的二季度持仓报告
+fund_005051 = [
+    {
+        "stock_code": "00883",
+        "stock_name": "中国海洋石油",
+        "position": 0.0396
+    },
+    {
+        "stock_code": "00857",
+        "stock_name": "中国石油股份",
+        "position": 0.0336
+    },
+    {
+        "stock_code": "01088",
+        "stock_name": "中国神华",
+        "position": 0.0334
+    },
+    {
+        "stock_code": "00386",
+        "stock_name": "中国石油化工股份",
+        "position": 0.0281
+    },
+    {
+        "stock_code": "00998",
+        "stock_name": "中信银行",
+        "position": 0.0271
+    },
+    {
+        "stock_code": "03618",
+        "stock_name": "重庆农村商业银行",
+        "position": 0.0263
+    },
+    {
+        "stock_code": "00914",
+        "stock_name": "海螺水泥",
+        "position": 0.0245
+    },
+    {
+        "stock_code": "00939",
+        "stock_name": "建设银行",
+        "position": 0.0238
+    },
+    {
+        "stock_code": "00762",
+        "stock_name": "中国联通",
+        "position": 0.0236
+    },
+    {
+        "stock_code": "03328",
+        "stock_name": "交通银行",
+        "position": 0.0231
+    },
+    {
+        "stock_code": "01766",
+        "stock_name": "中国中车",
+        "position": 0.0231
+    },
+    {
+        "stock_code": "03988",
+        "stock_name": "中国银行",
+        "position": 0.0227
+    },
+    {
+        "stock_code": "01398",
+        "stock_name": "工商银行",
+        "position": 0.0221
+    },
+    {
+        "stock_code": "02601",
+        "stock_name": "中国太保",
+        "position": 0.0212
+    },
+    {
+        "stock_code": "00008",
+        "stock_name": "电讯盈科",
+        "position": 0.0204
+    },
+    {
+        "stock_code": "00144",
+        "stock_name": "招商局港口",
+        "position": 0.0200
+    },
+    {
+        "stock_code": "01186",
+        "stock_name": "中国铁建",
+        "position": 0.0200
+    },
+    {
+        "stock_code": "00270",
+        "stock_name": "粤海投资",
+        "position": 0.0199
+    },
+    {
+        "stock_code": "01658",
+        "stock_name": "邮储银行",
+        "position": 0.0198
+    },
+    {
+        "stock_code": "00267",
+        "stock_name": "中信股份",
+        "position": 0.0194
+    },
+    {
+        "stock_code": "01988",
+        "stock_name": "民生银行",
+        "position": 0.0192
+    },
+    {
+        "stock_code": "00941",
+        "stock_name": "中国移动",
+        "position": 0.0192
+    },
+    {
+        "stock_code": "01288",
+        "stock_name": "农业银行",
+        "position": 0.0186
+    },
+    {
+        "stock_code": "00659",
+        "stock_name": "新创建集团",
+        "position": 0.0186
+    },
+    {
+        "stock_code": "01816",
+        "stock_name": "中广核电力",
+        "position": 0.0181
+    },
+    {
+        "stock_code": "01336",
+        "stock_name": "新华保险",
+        "position": 0.0180
+    },
+    {
+        "stock_code": "00728",
+        "stock_name": "中国电信",
+        "position": 0.0178
+    },
+    {
+        "stock_code": "00358",
+        "stock_name": "江西铜业股份",
+        "position": 0.0175
+    },
+    {
+        "stock_code": "01339",
+        "stock_name": "中国人民保险集团",
+        "position": 0.0175
+    },
+    {
+        "stock_code": "00012",
+        "stock_name": "恒基地产",
+        "position": 0.0172
+    },
+    {
+        "stock_code": "00392",
+        "stock_name": "北京控股",
+        "position": 0.0167
+    },
+    {
+        "stock_code": "00576",
+        "stock_name": "浙江沪杭甬",
+        "position": 0.0163
+    },
+    {
+        "stock_code": "00288",
+        "stock_name": "万洲国际",
+        "position": 0.0145
+    },
+    {
+        "stock_code": "00001",
+        "stock_name": "长和",
+        "position": 0.0143
+    },
+    {
+        "stock_code": "00003",
+        "stock_name": "香港中华煤气",
+        "position": 0.0139
+    },
+    {
+        "stock_code": "03360",
+        "stock_name": "远东宏信",
+        "position": 0.0138
+    },
+    {
+        "stock_code": "00489",
+        "stock_name": "东风集团股份",
+        "position": 0.0138
+    },
+    {
+        "stock_code": "02628",
+        "stock_name": "中国人寿",
+        "position": 0.0137
+    },
+    {
+        "stock_code": "01038",
+        "stock_name": "长江基建集团",
+        "position": 0.0136
+    },
+    {
+        "stock_code": "00006",
+        "stock_name": "电能实业",
+        "position": 0.0136
+    },
+    {
+        "stock_code": "01972",
+        "stock_name": "太古地产",
+        "position": 0.0123
+    },
+    {
+        "stock_code": "00016",
+        "stock_name": "新鸿基地产",
+        "position": 0.0119
+    },
+    {
+        "stock_code": "00019",
+        "stock_name": "太古股份公司A",
+        "position": 0.0116
+    },
+    {
+        "stock_code": "00101",
+        "stock_name": "恒隆地产",
+        "position": 0.0115
+    },
+    {
+        "stock_code": "02328",
+        "stock_name": "中国财险",
+        "position": 0.0115
+    },
+    {
+        "stock_code": "01099",
+        "stock_name": "国药控股",
+        "position": 0.0113
+    },
+    {
+        "stock_code": "00002",
+        "stock_name": "中电控股",
+        "position": 0.0113
+    },
+    {
+        "stock_code": "01044",
+        "stock_name": "恒安国际",
+        "position": 0.0113
+    },
+    {
+        "stock_code": "00017",
+        "stock_name": "新世界发展",
+        "position": 0.0101
+    },
+    {
+        "stock_code": "00151",
+        "stock_name": "中国旺旺",
+        "position": 0.0087
+    },
+    {
+        "stock_code": "09930",
+        "stock_name": "宏信建发",
+        "position": 0.0007
+    }
+]
+
+# 011584的二季度持仓报告
+fund_011584 = [
+    {
+        "stock_code": "02899",
+        "stock_name": "紫金矿业",
+        "latest_price": 18.40,
+        "position": 0.0792
+    },
+    {
+        "stock_code": "00883",
+        "stock_name": "中国海洋石油",
+        "latest_price": 20.40,
+        "position": 0.0576
+    },
+    {
+        "stock_code": "00700",
+        "stock_name": "腾讯控股",
+        "latest_price": 470.00,
+        "position": 0.0575
+    },
+    {
+        "stock_code": "03993",
+        "stock_name": "洛阳钼业",
+        "latest_price": 7.90,
+        "position": 0.0518
+    },
+    {
+        "stock_code": "03690",
+        "stock_name": "美团-W",
+        "latest_price": 197.20,
+        "position": 0.0514
+    },
+    {
+        "stock_code": "01818",
+        "stock_name": "招金矿业",
+        "latest_price": 14.26,
+        "position": 0.0505
+    },
+    {
+        "stock_code": "00005",
+        "stock_name": "汇丰控股",
+        "latest_price": 69.85,
+        "position": 0.0418
+    },
+    {
+        "stock_code": "09961",
+        "stock_name": "携程集团-S",
+        "latest_price": 514.50,
+        "position": 0.0383
+    },
+    {
+        "stock_code": "PDD",
+        "stock_name": "拼多多",
+        "latest_price": 145.64,
+        "position": 0.0368
+    },
+    {
+        "stock_code": "02282",
+        "stock_name": "美高梅中国",
+        "latest_price": 13.20,
+        "position": 0.0338
+    },
+    {
+        "stock_code": "03347",
+        "stock_name": "泰格医药",
+        "latest_price": 51.65,
+        "position": 0.0262
+    },
+    {
+        "stock_code": "02020",
+        "stock_name": "安踏体育",
+        "latest_price": 101.90,
+        "position": 0.0247
+    },
+    {
+        "stock_code": "09618",
+        "stock_name": "京东集团-SW",
+        "latest_price": 185.10,
+        "position": 0.0234
+    },
+    {
+        "stock_code": "06690",
+        "stock_name": "海尔智家",
+        "latest_price": 33.50,
+        "position": 0.0220
+    },
+    {
+        "stock_code": "01787",
+        "stock_name": "山东黄金",
+        "latest_price": 18.14,
+        "position": 0.0215
+    },
+    {
+        "stock_code": "02588",
+        "stock_name": "中银航空租赁",
+        "latest_price": 64.05,
+        "position": 0.0193
+    },
+    {
+        "stock_code": "09995",
+        "stock_name": "荣昌生物",
+        "latest_price": 18.84,
+        "position": 0.0169
+    },
+    {
+        "stock_code": "02162",
+        "stock_name": "康诺亚-B",
+        "latest_price": 43.95,
+        "position": 0.0163
+    },
+    {
+        "stock_code": "03668",
+        "stock_name": "兖煤澳大利亚",
+        "latest_price": 33.05,
+        "position": 0.0158
+    },
+    {
+        "stock_code": "02367",
+        "stock_name": "巨子生物",
+        "latest_price": 55.00,
+        "position": 0.0151
+    },
+    {
+        "stock_code": "09992",
+        "stock_name": "泡泡玛特",
+        "latest_price": 56.60,
+        "position": 0.0136
+    },
+    {
+        "stock_code": "01179",
+        "stock_name": "华住集团-S",
+        "latest_price": 32.05,
+        "position": 0.0134
+    },
+    {
+        "stock_code": "00392",
+        "stock_name": "北京控股",
+        "latest_price": 28.85,
+        "position": 0.0121
+    },
+    {
+        "stock_code": "06160",
+        "stock_name": "百济神州",
+        "latest_price": 143.60,
+        "position": 0.0109
+    },
+    {
+        "stock_code": "03311",
+        "stock_name": "中国建筑国际",
+        "latest_price": 11.50,
+        "position": 0.0104
+    },
+    {
+        "stock_code": "01138",
+        "stock_name": "中远海能",
+        "latest_price": 9.32,
+        "position": 0.0101
+    },
+    {
+        "stock_code": "01448",
+        "stock_name": "福寿园",
+        "latest_price": 4.66,
+        "position": 0.0100
+    },
+    {
+        "stock_code": "00916",
+        "stock_name": "龙源电力",
+        "latest_price": 7.02,
+        "position": 0.0090
+    },
+    {
+        "stock_code": "09896",
+        "stock_name": "名创优品",
+        "latest_price": 38.70,
+        "position": 0.0077
+    },
+    {
+        "stock_code": "TSM",
+        "stock_name": "台积电",
+        "latest_price": 172.07,
+        "position": 0.0070
+    },
+    {
+        "stock_code": "01415",
+        "stock_name": "高伟电子",
+        "latest_price": 23.20,
+        "position": 0.0050
+    },
+    {
+        "stock_code": "01919",
+        "stock_name": "中远海控",
+        "latest_price": 13.26,
+        "position": 0.0031
+    }
+]
+
+import requests
+
+def get_stock_data_us(stock_code):
+    url = f"http://hq.sinajs.cn/list={stock_code}"
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
+        'Referer': 'http://finance.sina.com.cn/'
+    }
+
+    response = requests.get(url, headers=headers)
+    if response.status_code == 200:
+        data = response.text
+        result = data[14:-3].replace('="',',').split(',')
+        return result[3]
+    else:
+        return f"Error: {response.status_code}"
+
+
+def get_stock_data(stock_code):
+    url = f"http://hq.sinajs.cn/list=hk{stock_code}"
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
+        'Referer': 'http://finance.sina.com.cn/'
+    }
+
+    response = requests.get(url, headers=headers)
+    if response.status_code == 200:
+        # 针对两支美股的特殊处理
+        if stock_code=="PDD":
+            return get_stock_data_us("gb_pdd")
+        elif stock_code=="TSM":
+            return get_stock_data_us("gb_tsm")
+
+        data = response.text.split(',')
+        return data[8]
+    else:
+        return f"Error: {response.status_code}"
+
+
+# 遍历基金数据，获取每个股票的涨跌幅并添加到fund_005051中
+for stock in fund_005051:
+    stock_code = stock['stock_code']
+    change = get_stock_data(stock_code)
+    stock['change'] = change
+
+for stock in fund_011584:
+    stock_code = stock['stock_code']
+    change = get_stock_data(stock_code)
+    stock['change'] = change
+
+fund_change_005051 = 0.0
+fund_change_011584 = 0.0
+
+for stock in fund_005051:
+    fund_change_005051 += stock['position'] * float(stock['change'])
+
+for stock in fund_011584:
+    fund_change_011584 += stock['position'] * float(stock['change'])
+
+print("fund_change_005051: ", fund_change_005051)
+print("fund_change_011584: ", fund_change_011584)
